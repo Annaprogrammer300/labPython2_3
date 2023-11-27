447684db2d8e97e6a2a182b01014aa786efe7abd
Annaprogrammer300 <star2004.as@gmail.com>
2023-11-27 18:00:44 +0400

    vers1.2

--- lab-2-1.py
+++ lab-2-1.py
@@ -45,3 +45,27 @@ def get_relative_paths(num_mark: int, folder_name: str) -> List[str]:
         relative_paths.append(os.path.join(class_path, name))
     return relative_paths
 
+def make_csv(folder_name: str) -> None:
+    """
+    The function writes data to a csv file in the following format: absolute path, relative path, class label
+
+    Args:
+        folder_name (str): folder path with initial dataset
+    """
+    bar = IncrementalBar('Writting csv', max=5000)
+    f = open("paths.csv", 'w')
+    writer = csv.writer(f, delimiter=',', lineterminator='\n')
+    for i in range(1, 6):
+        absolute_paths = get_absolute_paths(i, f'{folder_name}')
+        relative_paths = get_relative_paths(i, f'{folder_name}')
+        for absolute_path, relative_path in zip(absolute_paths, relative_paths):
+            bar.next()
+            writer.writerow([absolute_path, relative_path, str(i)])
+
+
+def main() -> None:
+    make_csv('dataset')
+
+
+if __name__ == '__main__':
+    main()
